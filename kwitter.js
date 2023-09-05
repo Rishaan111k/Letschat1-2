57bd326b018c75f3a4ffd7e68239d6c88d4a398e
Rishaan111k <139289393+Rishaan111k@users.noreply.github.com>
2023-09-05 19:59:14 -0400

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCwTaEkMwRVK7RNRHHB25eBC2FZw2steI",
+    authDomain: "lets-chat-e3b05.firebaseapp.com",
+    databaseURL: "https://lets-chat-e3b05-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-e3b05",
+    storageBucket: "lets-chat-e3b05.appspot.com",
+    messagingSenderId: "773967420686",
+    appId: "1:773967420686:web:a7708cc524b030798f0b5f"
+  };
+  
+ 
+  firebase.initializeApp(firebaseConfig);
